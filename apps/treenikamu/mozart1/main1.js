02c625f63c79933713e835c3e2faa2bd247f7fdb
terdee <115904299+terdee@users.noreply.github.com>
2022-10-25 02:13:44 +0300

    Update main1.js

--- apps/treenikamu/mozart1/main1.js
+++ apps/treenikamu/mozart1/main1.js
@@ -12,7 +12,7 @@ const resetAction = document.getElementById("reset-timer");
 const timeoutAudio = document.getElementById("alarm_audio");
 
 // Default inital value of timer
-const defaultValue = 0.1 * 60;
+const defaultValue = 20 * 60;
 
 // variable to the time
 var countDownTime = defaultValue;
